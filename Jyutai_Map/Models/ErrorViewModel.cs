37f5992fe0a3b1bd2b904b58a337f0098a5f8f39
Mitsuki Yamaguchi <y-mitsuki-2620@occ.co.jp>
2026-05-19 14:58:20 +0900

    プロジェクトファイルを追加。

---
+++ Jyutai_Map/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Jyutai_Map.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
